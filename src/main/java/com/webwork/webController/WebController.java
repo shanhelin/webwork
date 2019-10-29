a197d51d50c1b83f5b9e204ef9c684627616648d
hyxw <2376539013@qq.com>
2019-10-29 12:46:31 +0800

    完成接口映射

--- src/main/java/com/webwork/webController/WebController.java
+++ src/main/java/com/webwork/webController/WebController.java
@@ -3,11 +3,34 @@ package com.webwork.webController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 描述：项目中网址的跳转链接接口
+ * author：yulin
+ * Create date 2019-10-23 17:02
+ */
 @Controller
 public class WebController {
 
-    @RequestMapping("index")
+    /**
+     * 描述：主界面接口
+     * author：yulin
+     * Create date 2019-10-23
+     * @return
+     */
+    @RequestMapping("/index")
     public String index(){
         return "index.html";
     }
+
+    /**
+     * 描述：登录界面
+     * author：yulin
+     * Create date 2019-10-29 11:51
+     */
+    @RequestMapping("/login")
+    public String login(){
+        return "login.html";
+    }
+
+
 }
